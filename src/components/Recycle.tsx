7605d10297ec32c8569df152e29883689c30678f
Kurt <kurtbautista@gmail.com>
2023-04-30 17:35:36 +0800

    Optimize Recycle component

--- src/components/Recycle.tsx
+++ src/components/Recycle.tsx
@@ -3,25 +3,14 @@ import { useAppSelector } from "../hooks";
 import { robotsSelector } from "../store/slices/robotSlice";
 import { Robot } from "./Robot.types";
 import CheckedIcon from "@mui/icons-material/Check";
-import { headerTable } from "./helper";
+import { headerTable, transformData } from "./helper";
 
 import { SimpleTable } from "./index";
 
 const Recycle = () => {
   const { robots } = useAppSelector(robotsSelector);
 
-  const data = useMemo(
-    () =>
-      robots?.map(({ configuration, statuses, ...row }: Robot) => ({
-        ...row,
-        ...configuration,
-        hasSentience: configuration.hasSentience && <CheckedIcon />,
-        hasWheels: configuration.hasWheels && <CheckedIcon />,
-        hasTracks: configuration.hasTracks && <CheckedIcon />,
-        statuses: statuses.join(", "),
-      })),
-    []
-  );
+  const data = useMemo(() => transformData(robots), [robots]);
 
   return <SimpleTable header={headerTable} data={data} />;
 };
